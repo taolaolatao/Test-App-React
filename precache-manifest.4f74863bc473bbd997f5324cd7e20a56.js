ce0199c985289bf05698e476ca84aee47d47b5c3
vhm205 <minhhuynhvu9221@gmail.com>
2020-03-12 23:46:17 +0700

    Updates

---
+++ precache-manifest.4f74863bc473bbd997f5324cd7e20a56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d64efe0d5e95f4197c3b6206ffc25fcc",
+    "url": "/Test-App-React/index.html"
+  },
+  {
+    "revision": "d2b1ba47db226ea0299f",
+    "url": "/Test-App-React/static/css/main.f7f7ee8d.chunk.css"
+  },
+  {
+    "revision": "64160d351c025e68e664",
+    "url": "/Test-App-React/static/js/2.1aa83269.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/Test-App-React/static/js/2.1aa83269.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2b1ba47db226ea0299f",
+    "url": "/Test-App-React/static/js/main.bb960b7e.chunk.js"
+  },
+  {
+    "revision": "3371b5b70da3163c5585",
+    "url": "/Test-App-React/static/js/runtime-main.ba3acbfd.js"
+  }
+]);
